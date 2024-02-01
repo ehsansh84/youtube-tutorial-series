535df56fd88afa1983a11dafac5f6bb39381dc88
Ehsan <ehsan.shirzadi@gmail.com>
2024-02-01 22:21:46 +0100

    added: init

---
+++ Python/Shorts/02-__init__ Dunder method/main.py
@@ -0,0 +1,13 @@
+# __init__
+
+class Person:
+
+    def __init__(self, name, family):
+        self.name = name
+        self.family = family
+
+
+person = Person(name='Ehsan', family='Shirzadi')
+
+print(person.name)
+print(person.family)
